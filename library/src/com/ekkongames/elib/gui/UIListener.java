eaecc1579dd16e57b56de2fcc651ab82a5d0a0c5
Dolphish Dolphinium <dolphinspray.fr@gmail.com>
2016-06-21 16:03:17 -0400

    Updated the engine to keep track of events.

---
+++ library/src/com/ekkongames/elib/gui/UIListener.java
@@ -0,0 +1,32 @@
+package com.ekkongames.elib.gui;
+
+/**
+ * A <code>UIListener</code> is the base class of various event-triggered
+ * listeners. You may use these listeners to, for example, track whenever
+ * a password field is submitted.
+ */
+public abstract class UIListener {
+    public static final int MOUSE_MOVED = 1;
+    public static final int MOUSE_DRAGGED = 3;
+    public static final int MOUSE_PRESSED = 5;
+    public static final int MOUSE_RELEASED = 7;
+    public static final int MOUSE_CLICKED = 9;
+    public static final int KEY_PRESSED = 11;
+    public static final int KEY_RELEASED = 13;
+    public static final int KEY_TYPED = 15;
+    public static final int SUBMIT = 17;
+  
+  private final int trigger = 0;
+
+  /**
+   * Constructs a new <code>UIListener</code> with a trigger.
+   */
+  public UIListener(int trigger) {
+  }
+  
+  int getTrigger() {
+    return trigger;
+  }
+  
+  public abstract void onTrigger();
+}
